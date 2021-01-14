4afe88330735f971be38fe557f2be06e8bfcaccf
Marcos Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-01-14 02:32:07 -0300

    fourth lession - react native

---
+++ mobile/mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {useFonts, Nunito_600SemiBold,Nunito_700Bold,Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+  return (
+    <>
+     <StatusBar/>
+      <Routes/>
+    </>
+  );
+}
+
+
+
+
+
