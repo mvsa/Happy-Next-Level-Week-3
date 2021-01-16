abc17ae884d1df6362bd5125d7e7607c5089c734
Marcos Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-01-16 04:06:11 -0300

    final lession

--- mobile/mobile/src/routes.tsx
+++ mobile/mobile/src/routes.tsx
@@ -8,13 +8,36 @@ const {Navigator, Screen} = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanagesDetails from './pages/OrphanageDetails';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import Header from './components/Header';
 
 export default function Routes(){
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{headerShown: false, cardStyle:{backgroundColor:'#f2f3f5'}}}>
                 <Screen name = "OrphanagesMap" component={OrphanagesMap}/>
-                <Screen name = "OrphanageDetails" component={OrphanagesDetails}/>
+
+                <Screen 
+                 name = "OrphanageDetails" 
+                 component={OrphanagesDetails} 
+                 options={{
+                    headerShown: true,
+                    header: ()=><Header showCancel={false} title="Orfanato"/>}}/>
+
+                <Screen
+                 name = "OrphanageData"
+                 component={OrphanageData}
+                 options={{
+                    headerShown: true,
+                    header: ()=><Header title="Selecione no mapa"/>}}/>
+
+                <Screen 
+                 name = "SelectMapPosition"
+                 component={SelectMapPosition}
+                 options={{
+                    headerShown: true,
+                    header: ()=><Header title="Informe os dados"/>}}/>
             </Navigator>
         </NavigationContainer>
     )
